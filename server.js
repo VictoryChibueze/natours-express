018bd9f09018c21fa3a0d188e6189c08b819ab62
VictoryChibueze <alhajivicci12@gmail.com>
2024-08-29 11:26:03 +0100

    natours

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ dotenv.config({ path: './config.env' });
 const app = require('./index.js');
 
 // console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection sucessful');
+  });
+
 const Port = process.env.PORT || 3003;
 app.listen(Port, () => {
   console.log(`App is running on port ${Port}`);
