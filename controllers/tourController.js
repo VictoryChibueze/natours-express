c02ebd6f4679ff909231374a7aa23d3952d8abe5
VictoryChibueze <alhajivicci12@gmail.com>
2024-09-06 23:51:15 +0100

    natours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -57,17 +57,38 @@ exports.createTour = async (req, res) => {
 };
 
 exports.updateTour = async (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tours: `<updated tour here>`,
-    },
-  });
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(204).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndDelete(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
