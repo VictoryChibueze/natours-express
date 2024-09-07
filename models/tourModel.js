1717fae789bd6947ca32cb864e3222215375510f
VictoryChibueze <alhajivicci12@gmail.com>
2024-09-07 11:44:44 +0100

    natours

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,7 +5,26 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a name'],
     unique: true,
   },
-  rating: Number,
+  durations: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
 
   price: {
     type: Number,
